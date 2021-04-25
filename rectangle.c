34cfc99c85a128e65d7945a56472ab74b24d1e34
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-04-25 11:58:02 +0530

    Add files via upload

---
+++ rectangle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+  float l,b,area,peri;
+  printf("\n************************\n");
+  printf("Enter the length and breadth of rectangle: ");
+  scanf("%f%f",&l,&b);
+  peri=2*(l+b);
+  area=l*b;
+  printf("Area=%f\n",area);
+  printf("perimeter =%f\n",peri);
+  if(area>peri)
+  {
+    printf("area of rectangle is greater than perimeter");
+  }
+  else if(area<peri)
+  {
+    printf("area of rectangle is less than perimeter");
+  }
+  else
+  {
+    printf("area is equal to perimeter");
+  }
+  printf("\n************************\n\n");
+}
