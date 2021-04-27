9b39a741dfb95a24352db7cad0677f6bff8116e6
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-04-27 18:22:21 +0530

    Add files via upload

---
+++ price.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+  int s,c,p,l;
+  printf("\n************************\n");
+  printf("Enter the costing price: ");
+  scanf("%d",&c);
+  printf("Enter the selling price: ");
+  scanf("%d",&s);
+  if(s<c)
+  {
+    printf("Seller made a loss of");
+    l=c-s;
+    printf(" %d",l);
+  }
+  else if(s>c)
+  {
+    printf("Seller made a profit of");
+    p=s-c;
+    printf(" %d",p);
+  }
+  else
+  {
+    printf("Seller neither earns profit nor loss");
+  }
+  printf("\n************************\n\n");
+}
