321734142aaf1163fe08157e4bd04f3576b76bcc
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-04-27 18:26:20 +0530

    Update price.c

--- price.c
+++ price.c
@@ -3,22 +3,26 @@ void main()
 {
   int s,c,p,l;
   printf("\n************************\n");
-  printf("Enter the costing price: ");
+  printf("Enter the cost price: ");
   scanf("%d",&c);
+  
   printf("Enter the selling price: ");
   scanf("%d",&s);
+  
   if(s<c)
   {
     printf("Seller made a loss of");
     l=c-s;
     printf(" %d",l);
   }
+  
   else if(s>c)
   {
     printf("Seller made a profit of");
     p=s-c;
     printf(" %d",p);
   }
+  
   else
   {
     printf("Seller neither earns profit nor loss");
