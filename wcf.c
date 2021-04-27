46325a32e7fae1de601574935eca65ed6e65f7ea
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-04-27 18:27:08 +0530

    Update wcf.c

--- wcf.c
+++ wcf.c
@@ -7,8 +7,10 @@ void main()
   printf("\n************************\n");
   printf("Enter velocity in mph: ");
   scanf("%f",&v);
+  
   printf("Enter temperature in fahrenheit: ");
   scanf("%f",&t);
+  
   wcf=35.74+0.6215*t+(0.4275*t-35.75)*pow(v,0.6);
   printf("Wind chill factor = %f",wcf);
   printf("\n************************\n");
