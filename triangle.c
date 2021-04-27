c9b54de2a98cff4026906442f8b35e96d14752d6
ANJANA A <81566311+Anjana2002@users.noreply.github.com>
2021-04-27 18:27:42 +0530

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -6,12 +6,15 @@ void main()
   printf("\n************************\n");
   printf("Enter the three angles of a triangle: ");
   scanf("%f%f%f",&a,&b,&c);
+  
   //sum of the angles of triangle =180
   sum=a+b+c;
+  
   if(sum==180)
   {
     printf("It is a triangle");
   }
+  
   else
   {
     printf("It is not a triangle");
